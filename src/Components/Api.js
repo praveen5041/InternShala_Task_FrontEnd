023705dbe4436d24d7024efa3be2c449c5cec7c7
system <midathapallypraveen123@gmail.com>
2023-04-20 01:01:16 +0530

    api change

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -5,8 +5,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const appApi = createApi({
     reducerPath: "appApi",
     baseQuery: fetchBaseQuery({
-       //baseUrl: "https://giveup.onrender.com"
-        baseUrl:"https://chat-success.onrender.com"
+       baseUrl: "https://giveup.onrender.com"
+       // baseUrl:"https://chat-success.onrender.com"
         //baseUrl:"http://localhost:5000"
     }),
 
